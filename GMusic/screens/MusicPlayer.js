6b2b711b2e91503e2aaaf714e7b1114a6adbaa82
Carloss113 <karloskadu2706@gmail.com>
2026-08-25 12:27:33 -0300

    njcsailo

--- GMusic/screens/MusicPlayer.js
+++ GMusic/screens/MusicPlayer.js
@@ -1,21 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useState } from 'react'
+import { StyleSheet, Text, View, FlatList, Image, useWindowDimensions
+ } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import {songs} from '../model/data';
+
 import color from '../theme/color';
 
 export default function MusicPlayer() {
+  const { width } = useWindowDimensions();
+  const [selectIndex, setSelectIndex] = useState(0);
+
+  const currentsong = songs[selectIndex];
+  const artworkSize = Math.min(width - 40, 380);
+
+
+  function handleMomentumEnd(event) {
+    const offset = event.nativeEvent.contentOffset.x;
+    const index = Math.round(offset / width);
+    setSelectIndex(index);
+  }
+
+
+  function renderArtwork({ item }) {
+    return (
+      <View style={[styles.artworkPage, {width }]}>
+        <Image
+        source={item.artwork}
+          style={[styles.artwork, {width: artworkSize, height: artworkSize },
+            
+          ]}
+
+        />
+
+      </View>
+    )
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
-        <Text style={styles.eyebrown}>Tocando Agora</Text>
+        <Text style={styles.eyebrow}>TOCANDO AGORA</Text>
         <Text style={styles.title}>GMusic</Text>
-        <Text style={styles.description}> 
-            Nosso player começa aqui
+        <Text style={styles.description}>
+          Nosso player começa aqui
         </Text>
-
       </View>
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: color.background,
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  eyebrow: {
+    color: color.primary,
+    fontSize: 11,
+    fontWeight: '800',
+    letterSpacing: 1.8
+  },
+  title: {
+    marginTop: 8,
+    color: color.text,
+    fontSize: 32,
+    fontWeight: 800,
+  },
+  description: {
+    marginTop: 10,
+    color: color.textSecondary,
+  }
+})
